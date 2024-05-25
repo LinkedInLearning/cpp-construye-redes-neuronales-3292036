dfb1d661991c1697db05d97965b54def7ac7d1b4
Eliezer López <eliezer.lopezrodriguez@alum.uca.es>
2024-05-25 13:30:10 +0200

    Archivos del curso
    Archivos del curso "C++: Construye Redes Neuronales", impartido por @EliezerLopez y publicado en la biblioteca de cursos en español de LinkedIn Learning.

---
+++ src/03_02/a/PerceptronMulticapa.h
@@ -0,0 +1,30 @@
+/*
+ * Curso: C++ Construye redes neuronales
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
+#include <time.h>
+#include <cmath>
+
+class Perceptron{
+public:
+    Perceptron::Perceptron(size_t numero_de_entradas, double bias=1.0);
+    double ejecutar(std::vector<double> datos_de_entrada);
+    void establecer_pesos(std::vector<double> pesos_iniciales);
+    double sigmoide(double x);
+    std::vector<double> pesos;
+    double bias;
+};
+
+class PerceptronMulticapa{
+public:
+    std::vector<std::vector<Perceptron> > red;
+    std::vector<size_t> capas;   
+    std::vector<std::vector<double> > valores;
+    double bias;
+};
